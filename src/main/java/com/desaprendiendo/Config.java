3349f9604f63629bd84ce1f3413950c88f56732e
harold <harold@dev01>
2021-06-01 21:48:53 +0200

    Configuration for icefaces 4

---
+++ src/main/java/com/desaprendiendo/Config.java
@@ -0,0 +1,10 @@
+package com.desaprendiendo;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@FacesConfig
+@ApplicationScoped
+public class Config {
+
+}
